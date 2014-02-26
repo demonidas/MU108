959faaf16bf7244c0ca7b85b03669b983151b5a5
demonidas <dimko1991@gmail.com>
2014-02-26 05:31:44 +0200

    Dmitry Baranov HW2
    this is my HW2

---
+++ HW2/HW2/MainViewController.m
@@ -0,0 +1,33 @@
+//
+//  MainViewController.m
+//  HW2
+//
+//  Created by Dmitry on 26.02.14.
+//  Copyright (c) 2014 Dmitry Baranov. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)actionGenerateHoroscope:(id)sender {
+    
+    self.welcomeHoroscopeLabel.text = [NSString stringWithFormat:@"Приветствую, о прекрасный мир!"];
+}
+
+@end
---
+++ HW2/HW2Tests/HW2Tests.m
@@ -0,0 +1,34 @@
+//
+//  HW2Tests.m
+//  HW2Tests
+//
+//  Created by Dmitry on 26.02.14.
+//  Copyright (c) 2014 Dmitry Baranov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HW2Tests : XCTestCase
+
+@end
+
+@implementation HW2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
